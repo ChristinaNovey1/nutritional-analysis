99143b27284a408649d0da4ddf3447f7e509fffe
ChristinaNovey1 <christinanovey@gmail.com>
2025-11-16 11:33:43 -0800

    Update consts.py
    Added array of the feature names that I used in standardizer.py to standardize the food nutrition data

--- consts.py
+++ consts.py
@@ -37,6 +37,43 @@ feature_names = [
 "Zinc" #34
 ]
 
+standardized_feature_names = [
+    "Food", #0
+    "Calories", #1
+    "Fat Density", #2
+    "Saturated Fats Density", #3
+    "Monounsaturated Fats Density", #4
+    "Polyunsaturated Fats Density", #5
+    "Carbohydrates Density", #6
+    "Sugars Density", #7
+    "Protein Density", #8
+    "Dietary Fiber Density", #9
+    "Cholesterol Density", #10
+    "Sodium Density", #11
+    "Water Density",  #12
+    "Vitamin A Density", #13
+    "Vitamin B1 Density", #14
+    "Vitamin B11 Density", #15
+    "Vitamin B12 Density", #16
+    "Vitamin B2 Density", #17
+    "Vitamin B3 Density", #18
+    "Vitamin B5 Density", #19
+    "Vitamin B6 Density", #20
+    "Vitamin C Density", #21
+    "Vitamin D Density", #22
+    "Vitamin E Density", #23
+    "Vitamin K Density", #24
+    "Calcium Density", #25
+    "Copper Density", #26
+    "Iron Density", #27
+    "Magnesium Density", #28
+    "Manganese Density", #29
+    "Phosphorus Density", #30
+    "Potassium Density", #31
+    "Selenium Density", #32
+    "Zinc Density" #33
+]
+
 thresholds = {
 
     "Caloric_Value":        ("<", 200, 2),
